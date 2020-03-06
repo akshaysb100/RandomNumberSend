83615bf9f877195cdd334cd9652cdff43239986a
Akshay Sanjay Bavalekar <akshaysb100@Akshays-MacBook-Pro.local>
2020-03-06 14:18:18 +0530

    Android Service for random number

---
+++ app/src/main/java/com/example/androidservice/NextPage.kt
@@ -0,0 +1,12 @@
+package com.example.androidservice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NextPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_next_page)
+    }
+}
